06a4f044a0b393dc0bbc12089342da347b7cce8e
Afiq Zafry <Afiq Zafry@LAPTOP-3GH8R21A>
2024-05-23 17:50:16 +0800

    Setup mysql configuration

---
+++ Hacking_The_Future/src/main/java/Account/MySQLConfiguration.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Account;
+
+/**
+ *
+ * @author Afiq Zafry
+ */
+public class MySQLConfiguration {
+    public static final String url = "jdbc:mysql://localhost:3306/datastructure";
+    public static final String pass ="root";
+    
+    //You can double "/" my pass line code when you want to use your own password. 
+    //Use code below everytime you want torun
+    //public static final String pass ="";
+}
