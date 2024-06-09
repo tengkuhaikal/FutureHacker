639c905eb43b05ee7bb3c136fe7a968797f0ca92
USER <haikaltengku836@gmail.com>
2024-06-09 20:21:17 +0800

    reformat

--- Hacking_The_Future/src/main/java/Account/MySQLConfiguration.java
+++ Hacking_The_Future/src/main/java/Account/MySQLConfiguration.java
@@ -9,9 +9,10 @@ package Account;
  * @author Afiq Zafry
  */
 public class MySQLConfiguration {
+
     public static final String url = "jdbc:mysql://localhost:3306/datastructure";
-    public static final String pass ="root";
-    
+    public static final String pass = "root";
+
     //You can double "/" my pass line code when you want to use your own password. 
     //Use code below everytime you want torun
     //public static final String pass ="";
