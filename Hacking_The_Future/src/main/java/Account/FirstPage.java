7df3ba0be034e39156232519f410db1fa93def1f
USER <haikaltengku836@gmail.com>
2024-06-09 20:22:29 +0800

    reformat

--- Hacking_The_Future/src/main/java/Account/FirstPage.java
+++ Hacking_The_Future/src/main/java/Account/FirstPage.java
@@ -11,61 +11,61 @@ import java.util.Scanner;
  * @author Afiq Zafry
  */
 public class FirstPage {
+
     Scanner scan = new Scanner(System.in);
-    public int ui(){
+
+    public int ui() {
         System.out.println("\n\n----------------------------------------------");
         System.out.println("Welcome to Hacking The Future !");
-        
+        System.out.println("----------------------------------------------\n");
+
         System.out.println("1.Register an account");
         System.out.println("2.Login to your account");
         System.out.println("3.Exit");
-        System.out.print("Please select on what you want to do: ");
+        System.out.print("Option: ");
         int userInput = scan.nextInt();
         scan.nextLine();
         return userInput;
     }
-    
-    public User acc(int i){
-        boolean success=false;
-        switch(i){
+
+    public User acc(int i) {
+        boolean success = false;
+        switch (i) {
             case 1 -> {
-                
-                       return   Register.registernewuser();
-                         
+
+                return Register.registernewuser();
+
             }
             case 2 -> {
                 Login l = new Login();
-                return   l.lgin();
+                return l.lgin();
             }
             case 3 -> {
-                
-                System.out.println("Bye");
-                 System.exit(0);
+                System.out.println("Have a nice day! Goodbye!");
+                System.exit(0);
             }
         }
         return null;
     }
-    
-    public  User welcome(){
-        
+
+    public User welcome() {
+
         User user = acc(ui());
-        if(user==null){
-            return  welcome();
+        if (user == null) {
+            return welcome();
         }
-        
+
         return user;
-        
-       
+
     }
-    
+
     public static void main(String[] args) {
-      FirstPage fp = new FirstPage();
-     Login l = new Login();
-      AccountSettings as = new AccountSettings();
-    User user = fp.welcome();
-      //l.viewprofile(fp.welcome());
-      as.Settings(fp.welcome());
-     
-      
+        FirstPage fp = new FirstPage();
+        Login l = new Login();
+        AccountSettings as = new AccountSettings();
+        User user = fp.welcome();
+        //l.viewprofile(fp.welcome());
+        as.Settings(fp.welcome());
+
     }
 }
