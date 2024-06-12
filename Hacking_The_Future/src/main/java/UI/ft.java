18f28c2812891eaa6dbb153ca22726ca30652ba1
USER <haikaltengku836@gmail.com>
2024-06-12 21:09:07 +0800

    REFORMAT THE UI UNTIL DISCUSSION PAGE

---
+++ Hacking_The_Future/src/main/java/UI/ft.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI;
+
+/**
+ *
+ * @author tmhta_
+ */
+public class ft {
+    
+    public static void ft(String title) {
+        int lineLength = title.length() + 15; // Length of the line of dashes
+        int padding = (lineLength - title.length()) / 2;
+        String line = new String(new char[lineLength]).replace("", "-");
+        System.out.println("\n" + line);
+        System.out.println(String.format("%" + padding + "s%s%" + padding + "s", "", title, ""));
+        System.out.println(line + "\n");
+    }
+    
+    public static void error(String errorMessage){
+        System.out.println("!!! " + errorMessage + " !!!");
+    }
+    
+    public static void message(String message){
+        System.out.println(">>> " + message + " <<<");
+    }
+    
+}
